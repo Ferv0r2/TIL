3fb6cee907e90155791da18b7b18a6ae0ebd7ea8
황원태 <amlk31255@gmail.com>
2021-08-23 11:46:24 +0900

    merge Language

--- numeric_operators.js
+++
@@ -1,6 +0,0 @@
-console.log(2 + 1) // 3을 출력
-console.log(2 - 1) // 1을 출력
-console.log(4 / 2) // 2를 출력
-console.log(2 * 3) // 6을 출력
-console.log(10 % 3) // 나머지(remainder) 연산자. 1을 출력
-console.log(10 ** 2) // exponentiation. 10의 2승인 100을 출력
