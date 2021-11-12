cc2369591151eaaffabf2fe6a57ff0bb7d212398
황원태 <amlk31255@gmail.com>
2021-11-12 15:50:43 +0900

    Klaytn Wallet API

---
+++ Python/car.py
@@ -0,0 +1,26 @@
+class Car:
+    def __init__(self, distance, wheels, handle, excel, brake):
+        self.distance = 0
+        self.wheels = []
+        self.handle = handle
+        self.excel = excel
+        self.brake = brake
+
+    def handle(self, angle):
+        pass
+
+    def excel(self, btn):
+        pass
+    
+    def movement(self, time):
+        pass
+    
+    def brake(self, btn):
+        pass
+
+class Wheel:
+    pass
+
+# main
+
+myCar = Car()
